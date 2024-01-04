c7809bfa100c5fa8603f64f0d3d11955ba07c9db
diego magalhaes <digengup@gmail.com>
2024-01-04 12:54:55 -0300

    chore: create a config to local mongodb

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,11 +3,14 @@ import { taskModel } from './models'
 
 export const connectToDatabase = async (): Promise<void> => {
   try {
-    const mongoURI = 'mongodb://task:admin@localhost:27017'
+    const mongoURI =
+      process.env.MONGO_URI ??
+      'mongodb://root:changeme@localhost:27017/task-management-microservice?authSource=admin'
+
     await mongoose.connect(mongoURI)
     console.log('Database connection established')
     // If the tasks collection is empty it will be seeded with mock data during the first connection.
-    if (await taskModel.countDocuments() === 0) {
+    if ((await taskModel.countDocuments()) === 0) {
       // await taskModel.insertMany(tasksSeed)
       console.log('Database seeded with 28 random tasks')
     }
