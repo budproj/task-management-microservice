71f52dfbc46ad235cd566dad2cb21406f8d23976
Rodrigo Antas <rodr.antas@gmail.com>
2024-02-22 16:20:27 -0300

    feat(task-update): adding status

--- src/routes/task-updates/Service.ts
+++ src/routes/task-updates/Service.ts
@@ -23,7 +23,8 @@ export class TaskUpdatesService extends AbstractService<ITaskUpdate> implements 
       owner: task.owner,
       description: task.description,
       supportTeam: task.supportTeamMembers,
-      author
+      author,
+      status: task.status
     }
 
     return await this.repository.create({
@@ -38,13 +39,16 @@ export class TaskUpdatesService extends AbstractService<ITaskUpdate> implements 
   public async createTaskUpdates (oldTask: ITask, newTask: Partial<ITask>): Promise<ITaskUpdate> {
     const author = { type: IAuthorType.USER, identifier: oldTask.owner }
 
+    console.log({ newTask })
+
     const oldTaskstate = {
       title: oldTask.title,
       priority: oldTask.priority,
       dueDate: oldTask.dueDate,
       owner: oldTask.owner,
       description: oldTask.description,
-      supportTeam: oldTask.supportTeamMembers
+      supportTeam: oldTask.supportTeamMembers,
+      status: oldTask.status
     }
 
     const newTaskState = {
@@ -53,10 +57,11 @@ export class TaskUpdatesService extends AbstractService<ITaskUpdate> implements 
       dueDate: newTask.dueDate ?? oldTask.dueDate,
       owner: newTask.owner ?? oldTask.owner,
       description: newTask.description ?? oldTask.description,
-      supportTeam: newTask.supportTeamMembers ?? oldTask.supportTeamMembers
+      supportTeam: newTask.supportTeamMembers ?? oldTask.supportTeamMembers,
+      status: newTask.status ?? oldTask.status
     }
 
-    const updatePatches: ITaskPatchInterface[] = Object.keys(newTask).map((key) => ({
+    const updatePatches: ITaskPatchInterface[] = Object.keys(newTask).filter(key => key !== '_id').map((key) => ({
       // tipos precisam ser revistos
       key: TaskPatchsKeys[key as keyof typeof TaskPatchsKeys],
       value: (newTask as any)[key]
