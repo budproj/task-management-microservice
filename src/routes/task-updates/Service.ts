078319c6479365b05e25847d66b6fb83d5798fa7
Igor Omote <igor.omote@gmail.com>
2024-03-13 14:37:39 -0300

    clean unused import

--- src/routes/task-updates/Service.ts
+++ src/routes/task-updates/Service.ts
@@ -6,7 +6,6 @@ import { ITaskUpdatesService } from '../../ts/interfaces/routes/task-updates'
 import { randomUUID } from 'crypto'
 import { User } from '../../ts/interfaces/entities/users/User'
 import { boardModel } from '../../database/models'
-import { Team } from '../../ts/interfaces/entities/team/Team'
 import AmqpConnection from '../messaging/amqp-connection'
 
 export class TaskUpdatesService extends AbstractService<ITaskUpdate> implements ITaskUpdatesService {
