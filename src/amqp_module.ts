921848f583b2dbe29465d7d361bafa43e07ff676
Igor Omote <igoromote@github.com>
2024-03-18 11:31:13 -0300

    connect right

--- src/amqp_module.ts
+++ src/amqp_module.ts
@@ -32,7 +32,30 @@ export default async function setupAMQP (): Promise<void> {
         }
         amqpSender.sendMessage('notifications-microservice.notification', notification) as any
         reject(err)
-      } else resolve(conn)
+      } else {
+        const amqpSender = new AmqpConnection()
+        const notification = {
+          messageId: randomUUID(),
+          type: 'provingSomething',
+          timestamp: new Date().toISOString(),
+          recipientId: 'auth0|auth0|6243762bdf154e0068d272d7',
+          properties: {
+            sender: {
+              id: 'auth0|auth0|6243762bdf154e0068d272d7',
+              name: 'Igor Omote',
+              picture: 'https://s3-sa-east-1.amazonaws.com/business.s3.getbud.co/user/pictures/335cd9ee-e5df-402c-a268-6c7a96ee7801-1657539472265.jpeg'
+            },
+            task: {
+              id: '65e08a748b491e52ee118057',
+              name: 'connectOK'
+            },
+            taskBoard: { _id: '65f83ef2275dbdbe1328502f', owner: '825112f5-6da7-4d92-a845-79a3ea355fd4', title: 'bom dia sua máquina de construir músculo', author: { type: 'USER', identifier: '825112f5-6da7-4d92-a845-79a3ea355fd4' }, status: 'toDo', boardId: '65c6585c4ae33e74c9c49a4d', dueDate: '1112-11-11T03:06:28.000Z', priority: 4, description: '<p>bom dia sua máquina de construir músculo</p>', initialDate: '1112-11-11T03:06:28.000Z', supportTeam: [] },
+            teamId: '0342b8f6-3a07-4f2b-a3fa-a3a8ca8fa61f'
+          }
+        }
+        amqpSender.sendMessage('notifications-microservice.notification', notification) as any
+        resolve(conn)
+      }
     })
   })
   const channel = await new Promise<amqp.Channel>((resolve, reject) => {
