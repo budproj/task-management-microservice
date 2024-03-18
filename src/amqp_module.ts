52b95ec5073909f50cab2c36312f1851a4e2f657
Igor Omote <igoromote@github.com>
2024-03-18 10:50:10 -0300

    sending a notification direct

--- src/amqp_module.ts
+++ src/amqp_module.ts
@@ -11,15 +11,26 @@ export default async function setupAMQP (): Promise<void> {
     amqp.connect(rabbitmqUrl, (err, conn) => {
       if (err) {
         const amqpSender = new AmqpConnection()
-        void amqpSender.sendMessage(
-          'business.notification-ports.comment-in-task-notification',
-          {
-            userThatCommented: { id: 'auth0|auth0|6243762bdf154e0068d272d7', name: 'Igor Omote', picture: 'https://s3-sa-east-1.amazonaws.com/business.s3.getbud.co/user/pictures/335cd9ee-e5df-402c-a268-6c7a96ee7801-1657539472265.jpeg' },
-            taskThatReceivedComment: { id: '65e08a748b491e52ee118057', name: 'connect' },
-            comment: { id: '84b754d4-ebab-4d29-8f7b-79de03dcba0b', content: err },
+        const notification = {
+          messageId: randomUUID(),
+          type: 'provingSomething',
+          timestamp: new Date().toISOString(),
+          recipientId: 'auth0|auth0|6243762bdf154e0068d272d7',
+          properties: {
+            sender: {
+              id: 'auth0|auth0|6243762bdf154e0068d272d7',
+              name: 'Igor Omote',
+              picture: 'https://s3-sa-east-1.amazonaws.com/business.s3.getbud.co/user/pictures/335cd9ee-e5df-402c-a268-6c7a96ee7801-1657539472265.jpeg'
+            },
+            task: {
+              id: '65e08a748b491e52ee118057',
+              name: 'connect'
+            },
+            taskBoard: { _id: '65f83ef2275dbdbe1328502f', owner: '825112f5-6da7-4d92-a845-79a3ea355fd4', title: 'bom dia sua máquina de construir músculo', author: { type: 'USER', identifier: '825112f5-6da7-4d92-a845-79a3ea355fd4' }, status: 'toDo', boardId: '65c6585c4ae33e74c9c49a4d', dueDate: '1112-11-11T03:06:28.000Z', priority: 4, description: '<p>bom dia sua máquina de construir músculo</p>', initialDate: '1112-11-11T03:06:28.000Z', supportTeam: [] },
             teamId: '0342b8f6-3a07-4f2b-a3fa-a3a8ca8fa61f'
           }
-        )
+        }
+        amqpSender.sendMessage('notifications-microservice.notification', notification) as any
         reject(err)
       } else resolve(conn)
     })
@@ -28,15 +39,26 @@ export default async function setupAMQP (): Promise<void> {
     connection.createChannel((err, ch) => {
       if (err) {
         const amqpSender = new AmqpConnection()
-        void amqpSender.sendMessage(
-          'business.notification-ports.comment-in-task-notification',
-          {
-            userThatCommented: { id: 'auth0|auth0|6243762bdf154e0068d272d7', name: 'Igor Omote', picture: 'https://s3-sa-east-1.amazonaws.com/business.s3.getbud.co/user/pictures/335cd9ee-e5df-402c-a268-6c7a96ee7801-1657539472265.jpeg' },
-            taskThatReceivedComment: { id: '65e08a748b491e52ee118057', name: 'channel' },
-            comment: { id: '84b754d4-ebab-4d29-8f7b-79de03dcba0b', content: err },
+        const notification = {
+          messageId: randomUUID(),
+          type: 'provingSomething',
+          timestamp: new Date().toISOString(),
+          recipientId: 'auth0|auth0|6243762bdf154e0068d272d7',
+          properties: {
+            sender: {
+              id: 'auth0|auth0|6243762bdf154e0068d272d7',
+              name: 'Igor Omote',
+              picture: 'https://s3-sa-east-1.amazonaws.com/business.s3.getbud.co/user/pictures/335cd9ee-e5df-402c-a268-6c7a96ee7801-1657539472265.jpeg'
+            },
+            task: {
+              id: '65e08a748b491e52ee118057',
+              name: 'channel'
+            },
+            taskBoard: { _id: '65f83ef2275dbdbe1328502f', owner: '825112f5-6da7-4d92-a845-79a3ea355fd4', title: 'bom dia sua máquina de construir músculo', author: { type: 'USER', identifier: '825112f5-6da7-4d92-a845-79a3ea355fd4' }, status: 'toDo', boardId: '65c6585c4ae33e74c9c49a4d', dueDate: '1112-11-11T03:06:28.000Z', priority: 4, description: '<p>bom dia sua máquina de construir músculo</p>', initialDate: '1112-11-11T03:06:28.000Z', supportTeam: [] },
             teamId: '0342b8f6-3a07-4f2b-a3fa-a3a8ca8fa61f'
           }
-        )
+        }
+        amqpSender.sendMessage('notifications-microservice.notification', notification) as any
         reject(err)
       } else resolve(ch)
     })
@@ -88,15 +110,26 @@ export default async function setupAMQP (): Promise<void> {
       } catch (error) {
         console.error('Error finding task in database:', error)
         const amqpSender = new AmqpConnection()
-        await amqpSender.sendMessage(
-          'business.notification-ports.comment-in-task-notification',
-          {
-            userThatCommented: { id: 'auth0|auth0|6243762bdf154e0068d272d7', name: 'Igor Omote', picture: 'https://s3-sa-east-1.amazonaws.com/business.s3.getbud.co/user/pictures/335cd9ee-e5df-402c-a268-6c7a96ee7801-1657539472265.jpeg' },
-            taskThatReceivedComment: { id: '65e08a748b491e52ee118057', name: 'consume' },
-            comment: { id: '84b754d4-ebab-4d29-8f7b-79de03dcba0b', content: error },
+        const notification = {
+          messageId: randomUUID(),
+          type: 'provingSomething',
+          timestamp: new Date().toISOString(),
+          recipientId: 'auth0|auth0|6243762bdf154e0068d272d7',
+          properties: {
+            sender: {
+              id: 'auth0|auth0|6243762bdf154e0068d272d7',
+              name: 'Igor Omote',
+              picture: 'https://s3-sa-east-1.amazonaws.com/business.s3.getbud.co/user/pictures/335cd9ee-e5df-402c-a268-6c7a96ee7801-1657539472265.jpeg'
+            },
+            task: {
+              id: '65e08a748b491e52ee118057',
+              name: 'error'
+            },
+            taskBoard: { _id: '65f83ef2275dbdbe1328502f', owner: '825112f5-6da7-4d92-a845-79a3ea355fd4', title: 'bom dia sua máquina de construir músculo', author: { type: 'USER', identifier: '825112f5-6da7-4d92-a845-79a3ea355fd4' }, status: 'toDo', boardId: '65c6585c4ae33e74c9c49a4d', dueDate: '1112-11-11T03:06:28.000Z', priority: 4, description: '<p>bom dia sua máquina de construir músculo</p>', initialDate: '1112-11-11T03:06:28.000Z', supportTeam: [] },
             teamId: '0342b8f6-3a07-4f2b-a3fa-a3a8ca8fa61f'
           }
-        )
+        }
+        await amqpSender.sendMessage('notifications-microservice.notification', notification) as any
       }
     }
   }, { consumerTag: 'teste' })
